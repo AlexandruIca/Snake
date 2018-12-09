29f74213b1fac3714947df2434d77846577b7638
Alexandru Ica <alexandruica703@gmail.com>
2018-12-09 20:08:57 +0200

    Deleted .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++
@@ -1,8 +0,0 @@
-def FlagsForFile(fileName, **kwargs):
-    return { 'flags': [ '-std=c++17',
-                        '-Wall',
-                        '-Werror',
-                        '-Wextra',
-                        '-Wpedantic' ] }
-
-
